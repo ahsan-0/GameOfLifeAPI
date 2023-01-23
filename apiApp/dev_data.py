5a038580dd0586e275f71d64d5dcde52cacf7f5d
ahsan-0 <ashanb995@gmail>
2023-01-23 16:37:38 +0000

    create and add data to dev_data file

---
+++ apiApp/dev_data.py
@@ -0,0 +1,29 @@
+import datetime
+patterns = [
+    {
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [1, 1, 1, 1]],
+        "pattern_name": "seaboat",
+        "username": "john123",
+        'created_at':datetime.datetime.utcnow()
+    },
+    {
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 0, 1]],
+        "pattern_name": "airplane",
+        "username": "bob12",
+    },
+    {
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [1, 1, 1, 0]],
+        "pattern_name": "lumberjack",
+        "username": "andy09",
+    },
+    {
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 1, 0, 1]],
+        "pattern_name": "fisherman",
+        "username": "zack81",
+    },
+    {
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 1, 1]],
+        "pattern_name": "python",
+        "username": "mark123",
+    },
+]
