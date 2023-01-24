af73ace4f8280d87f94d0897ebd7c87967c1adac
3782291211 <Laklouk98@hotmail.com>
2023-01-24 10:55:50 +0000

    Updating urls

--- apiApp/urls.py
+++ apiApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-  path('',views.get_patterns,name='get_patterns')
-       ]
+  path('patterns',views.get_patterns, name='get_patterns'),
+  path('patterns/<str:id>',views.get_single_pattern, name='get_single_pattern'),
+  path('users',views.get_users, name='get_users'),
+  ]
