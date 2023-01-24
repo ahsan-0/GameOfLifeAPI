a58bf1858ae51505f46ff729694b3b57365426a5
ahsan-0 <ashanb995@gmail>
2023-01-24 17:27:54 +0000

    add multiple patterns for a single user

--- apiApp/dev_data.py
+++ apiApp/dev_data.py
@@ -3,43 +3,25 @@ from datetime import datetime
 patterns = [
     {
         "username": "john123",
-        "pattern_name": "seaboat",
-        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [1, 1, 1, 1]],
-        "created_at": datetime.now(),
-    },
-    {
-        "username": "bob12",
-        "pattern_name": "airplane",
-        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 0, 1]],
-        "created_at": datetime.now(),
-    },
-    {
-        "username": "andy09",
-        "pattern_name": "lumberjack",
-        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [1, 1, 1, 0]],
-        "created_at": datetime.now(),
-    },
-    {
-        "username": "zack81",
-        "pattern_name": "fisherman",
-        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 1, 0, 1]],
-        "created_at": datetime.now(),
-    },
-    {
-        "username": "mark123",
-        "pattern_name": "python",
-        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 1, 1]],
-        "created_at": datetime.now(),
-    },
-    {
-        "username": "john123",
         "pattern_name": "orphous",
         "pattern_body": [[0, 1, 1, 1], [0, 1, 1, 1], [1, 1, 1, 0], [0, 1, 1, 1]],
+        "created_at": datetime.now(),
+    },
+     {
+        "username": "john123",
+        "pattern_name": "mango",
+        "pattern_body": [[0, 1, 1, 0], [0, 0, 0, 1], [0, 1, 1, 0], [0, 0, 1, 1]],
         "created_at": datetime.now(),
     },
     {
         "username": "bob12",
         "pattern_name": "bricklayer",
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 0, 1]],
+        "created_at": datetime.now(),
+    },
+     {
+        "username": "bob12",
+        "pattern_name": "layer",
         "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 0, 1]],
         "created_at": datetime.now(),
     },
