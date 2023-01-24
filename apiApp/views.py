621c1a498c5b7f448cfd65ff66a30e223e227057
ahsan-0 <ashanb995@gmail>
2023-01-24 09:20:05 +0000

    format code

--- apiApp/views.py
+++ apiApp/views.py
@@ -5,13 +5,19 @@ from rest_framework import status
 from pymongo import MongoClient
 from apiApp.models import find_patterns
 import json
+
 # Create your views here.
 client = MongoClient(
-        "mongodb+srv://GNA7R:eerDKGruC7PUqjyx@rootcluster.i0un9uw.mongodb.net/?retryWrites=true&w=majority")
-db = client['multiply_till_you_die_db']
-patterns_collection = db['patterns']
+    "mongodb+srv://GNA7R:eerDKGruC7PUqjyx@rootcluster.i0un9uw.mongodb.net/?retryWrites=true&w=majority"
+)
+db = client["multiply_till_you_die_db"]
+patterns_collection = db["patterns"]
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_patterns(request):
-  if request.method == 'GET':
-    return Response({'patterns':json.loads(find_patterns(request,patterns_collection))},status=status.HTTP_200_OK)
+    if request.method == "GET":
+        return Response(
+            {"patterns": json.loads(find_patterns(request, patterns_collection))},
+            status=status.HTTP_200_OK,
+        )
