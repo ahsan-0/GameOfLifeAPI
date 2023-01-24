ebffe3b9fb3c91e3841391460ca347d836bf4220
ahsan-0 <ashanb995@gmail>
2023-01-24 11:57:43 +0000

    refactor data to have correct username

--- apiApp/dev_data.py
+++ apiApp/dev_data.py
@@ -11,25 +11,55 @@ patterns = [
         "username": "bob12",
         "pattern_name": "airplane",
         "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 0, 1]],
-        "created_at": datetime.now()
+        "created_at": datetime.now(),
     },
     {
         "username": "andy09",
         "pattern_name": "lumberjack",
         "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [1, 1, 1, 0]],
-        "created_at": datetime.now()
+        "created_at": datetime.now(),
     },
     {
         "username": "zack81",
         "pattern_name": "fisherman",
         "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 1, 0, 1]],
-        "created_at": datetime.now()
+        "created_at": datetime.now(),
     },
     {
         "username": "mark123",
         "pattern_name": "python",
         "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 1, 1]],
-        "created_at": datetime.now()
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "john123",
+        "pattern_name": "orphous",
+        "pattern_body": [[0, 1, 1, 1], [0, 1, 1, 1], [1, 1, 1, 0], [0, 1, 1, 1]],
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "bob12",
+        "pattern_name": "bricklayer",
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 0, 1]],
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "andy09",
+        "pattern_name": "roblox",
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [1, 1, 1, 0]],
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "zack81",
+        "pattern_name": "fisherman",
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 1, 0, 1]],
+        "created_at": datetime.now(),
+    },
+    {
+        "username": "mark123",
+        "pattern_name": "atlas",
+        "pattern_body": [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 0], [0, 0, 1, 1]],
+        "created_at": datetime.now(),
     },
 ]
 
@@ -37,32 +67,32 @@ patterns = [
 users = [
     {
         "account_owner": "John Smith",
-        "username": "johnXXX333",
-        "email": "John@outlook.com",
-        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE"
+        "username": "john123",
+        "email": "john132@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
     },
-     {
-        "account_owner": "Emily Johnson",
-        "username": "EmzxXx",
-        "email": "emilyoutlook@outlook.com",
-        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE"
+    {
+        "account_owner": "Andy Jones",
+        "username": "andy09",
+        "email": "andy09@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
     },
-     {
-        "account_owner": "Max Varga",
-        "username": "vargu5637643",
-        "email": "maxv@outlook.com",
-        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE"
+    {
+        "account_owner": "Bob Job",
+        "username": "bob12",
+        "email": "bob12@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
     },
-     {
-        "account_owner": "Roy Watson",
-        "username": "roy_watson",
-        "email": "roywatson@outlook.com",
-        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE"
+    {
+        "account_owner": "Zack Roman",
+        "username": "zack81",
+        "email": "zack81@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
     },
-     {
-        "account_owner": "Billie George",
-        "username": "billie_32787",
-        "email": "billiegeorge@outlook.com",
-        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE"
-    }
+    {
+        "account_owner": "Mark iphone",
+        "username": "mark123",
+        "email": "mark123@outlook.com",
+        "avatar_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fus[…]e=images&cd=vfe&ved=0CA4QjRxqFwoTCLDL1Z2B4PwCFQAAAAAdAAAAABAE",
+    },
 ]
