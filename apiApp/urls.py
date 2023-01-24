25c9958ac41bb2bb9ea982879d8c1430b7a4f8fe
ahsan-0 <ashanb995@gmail>
2023-01-24 11:58:16 +0000

    add new url paths for username and patterns by username

--- apiApp/urls.py
+++ apiApp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns= [
-  path('patterns',views.get_patterns, name='get_patterns'),
-  path('patterns/<str:id>',views.get_single_pattern, name='get_single_pattern'),
-  path('users',views.get_users, name='get_users'),
-  ]
+urlpatterns = [
+    path("patterns", views.get_patterns, name="get_patterns"),
+    path("patterns/<str:id>", views.get_single_pattern, name="get_single_pattern"),
+    path("users", views.get_users, name="get_users"),
+    path("users/<str:username>", views.get_single_user, name="get_single_user"),
+    path('users/<str:username>/patterns',views.get_patterns_by_username, name='get_patterns_by_username')
+]
