91aa1b1ea27af5306cc3b7f92d04b8204bddbb02
ahsan-0 <ashanb995@gmail>
2023-01-24 15:41:17 +0000

    change parametric endpoint to user_id

--- apiApp/urls.py
+++ apiApp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("patterns", views.get_patterns, name="get_patterns"),
-    path("patterns/<str:id>", views.get_single_pattern, name="get_single_pattern"),
+    path("patterns/<str:id>", views.single_pattern, name="get_single_pattern"),
     path("users", views.get_users, name="get_users"),
-    path("users/<str:username>", views.get_single_user, name="get_single_user"),
-    path('users/<str:username>/patterns',views.get_patterns_by_username, name='get_patterns_by_username')
+    path("users/<str:id>", views.get_single_user, name="get_single_user"),
+    path('users/<str:id>/patterns',views.get_patterns_by_id, name='get_patterns_by_id')
 ]
