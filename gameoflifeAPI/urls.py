9d09f669ab70f165f38925a722914576babc47a8
3782291211 <Laklouk98@hotmail.com>
2023-01-24 10:56:05 +0000

    updating settings

--- gameoflifeAPI/urls.py
+++ gameoflifeAPI/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/patterns', include('apiApp.urls'))
+    path('api/', include('apiApp.urls'))
 ]
