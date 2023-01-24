ea8ceb182abc06329346c94cbd1cbf110102a338
ahsan-0 <ashanb995@gmail>
2023-01-24 09:19:24 +0000

    format code

--- apiApp/urls.py
+++ apiApp/urls.py
@@ -1,6 +1,4 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
-urlpatterns= [
-  path('',views.get_patterns,name='get_patterns')
-       ]
+urlpatterns = [path("", views.get_patterns, name="get_patterns")]
