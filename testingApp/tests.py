65e082f5dd3c2782cb657972a498fd2c7409601f
ahsan-0 <ashanb995@gmail>
2023-01-25 17:02:53 +0000

    add tests for requests

--- testingApp/tests.py
+++ testingApp/tests.py
@@ -33,6 +33,7 @@ class PatternsGetRequestTests(APITestCase):
 class PatternsPostRequestTests(APITestCase):
     patterns_url = reverse('test_get_patterns')
     def test_post_pattern(self):
+        seed_test_db()
         request_body = {
 			"username": "bob12",
 			"pattern_name": "special pattern",
@@ -74,4 +75,26 @@ class PatternsPostRequestTests(APITestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data['msg'], "Request body is missing a required key.")
 
-    
+    def test_post_duplicate_pattern(self):
+        request_body = {
+			"username": "bob12",
+			"pattern_name": "special pattern",
+			"pattern_body": "0101001101 1011001010 0011011010 1000101010 0100101000 1101100000 1010000011 1111000000 0100101010 0000101010",
+	    }
+        response = self.client.post(self.patterns_url,request_body,format='json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['msg'],'Pattern already exists in database.')
+        
+class PatternsPutRequestTest(APITestCase):
+    def test_put_new_pattern_name(self):
+        request_body = {
+            'pattern_name':'icecrystal'
+        }
+        patterns = self.client.get('/test/patterns').data['patterns']
+        first_pattern = patterns[0]
+        url_inc_id = reverse('test_get_single_pattern', args=[first_pattern['_id']])
+        response = self.client.put(url_inc_id, request_body, format='json')
+        self.assertEqual(response.status_code,202)
+        self.assertEqual(response.data['updated_pattern']['pattern_name'], 'icecrystal')
+        first_pattern.pop('pattern_name')
+        self.assertEqual(all(response.data['updated_pattern'].get(k)==v for k,v in first_pattern.items()),True)
