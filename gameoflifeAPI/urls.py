201b8bb9ab9eee9aca307901e4b71f90228cfa01
ahsan-0 <ashanb995@gmail>
2023-01-27 13:51:25 +0000

    remove admin url

--- gameoflifeAPI/urls.py
+++ gameoflifeAPI/urls.py
@@ -13,15 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from apiApp import views
 
 handler404 = 'apiApp.views.handler404'
 handler500 = 'apiApp.views.handler500'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('apiApp.urls')),
     path('test/', include('testingApp.urls'))
 ]
